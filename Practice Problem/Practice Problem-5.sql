4c5a79839168656e7659018052bbef11ccd28fd8
karan narula <karannarula370@gmail.com>
2023-10-03 15:37:43 +0530

    UD

---
+++ Practice Problem/Practice Problem-5.sql
@@ -0,0 +1,14 @@
+/*
+ write a SQL Query to shows the monthly order totals for each customer up to the 11th month of the year, sorted by CustomerID and OrderDate?"
+*/
+
+with CTE as
+(
+select CustomerID, OrderDate, sum(OrderTotal) as Amount, MONTH(OrderDate) as [Month] from [Sales].[Order]
+group by  CustomerID, OrderDate
+)
+
+select  *
+from CTE
+where [Month] <=11
+order by CustomerID, OrderDate
