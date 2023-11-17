761df6131114c97911c518369d3da53298de57aa
karan narula <karannarula370@gmail.com>
2023-11-17 16:37:40 +0530

    Uploaded data

--- Practice Problem/Practice problem-23 (Lead and Lag function).sql
+++ Practice Problem/Practice problem-23 (Lead and Lag function).sql
@@ -1,5 +1,6 @@
 
 /*
+order by clause is important in over()
 LAG(expression [,offset[,default_value]]) OVER(ORDER BY columns)
 LEAD(expression [,offset[,default_value]]) OVER(ORDER BY columns)
 */
@@ -15,18 +16,26 @@ order by OrderID
 
 
 select OrderID, CustomerID ,
-lag(OrderID) over (partition by CustomerID order by OrderID) as lag_1,
-lead(OrderID) over (partition by CustomerID order by OrderID asc) as lead_1
+lag(OrderID) over (partition by CustomerID order by OrderID) as Previous_Order,
+lead(OrderID) over (partition by CustomerID order by OrderID asc) as Next_Order
 from [Sales].[Order]
 order by CustomerID
 
+----this wont give an accurate result, end of orderid gives aggregate value but it would have been a null value	
 SELECT OrderID, CustomerID,
        LEAD(OrderID) OVER (PARTITION BY CustomerID ORDER BY OrderID ASC) AS lead_1
 FROM [Sales].[Order]
 ORDER BY OrderID;
 
+----Use Offset value
 SELECT OrderID, CustomerID,
-       LEAD(OrderID) OVER (PARTITION BY CustomerID ORDER BY OrderID ASC) AS lead_1
+       LEAD(OrderID,2) OVER (PARTITION BY CustomerID ORDER BY OrderID ASC) AS lead_1
+FROM [Sales].[Order]
+ORDER BY CustomerID
+
+----Use offset with default value
+SELECT OrderID, CustomerID,
+       LEAD(OrderID,2,-1) OVER (PARTITION BY CustomerID ORDER BY OrderID ASC) AS lead_1
 FROM [Sales].[Order]
 ORDER BY CustomerID
 
--- Practice Problem/Practice problem-24 (Lead and Lag function).sql
+++ Practice Problem/Practice problem-24 (Lead and Lag function).sql
@@ -2,6 +2,21 @@
 
 select * from [Sales].[Order]
 
+---Analyze the Previous Year Sales
+with CTE as
+(
+select YEAR(OrderDate) as [Year], 
+sum(OrderTotal) as Revenue
+from [Sales].[Order]
+group by  YEAR(OrderDate)
+)
+select [Year], format(Revenue,'C2') as Revenue, 
+format(lag(Revenue,1,0) over(order by [Year]), 'C2') PreviousYearRevenue
+from CTE
+
+/* 
+Analyzing Year-over-Year Revenue Growth
+*/
 with CTE as
 (
 select YEAR(OrderDate) as [Year], 
@@ -9,7 +24,30 @@ sum(OrderTotal) as Revenue
 from [Sales].[Order]
 group by  YEAR(OrderDate)
 ),
-CTE_1 as
-select [Year], format(Revenue,'C2') as Revenue, 
-format(lag(Revenue,1,0) over(order by [Year]), 'C2') PreviousYearRevenue
+CTE_1 as 
+(
+select [Year], Revenue, 
+lag(Revenue,1) over(order by [Year]) PreviousYearRevenue
 from CTE
+)
+select [Year], 
+format(Revenue,'C2')Revenue,
+format(lag(Revenue,1) over(order by [Year]), 'C2') PreviousYearRevenue,
+format(((Revenue - PreviousYearRevenue)/(PreviousYearRevenue)), 'P2') as YOY_Growth
+from
+CTE_1
+
+
+with CTE as
+(
+select YEAR(OrderDate) as [Year], 
+sum(OrderTotal) as Revenue
+from [Sales].[Order]
+group by  YEAR(OrderDate)
+)
+Select [Year], format(Revenue,'C2') as Revenue,
+format(lag(Revenue,1,Revenue) over(order by [Year]),'C2') as PreviousYearRevenue,
+format((Revenue - lag(Revenue,1,Revenue) over(order by [Year]))/(lag(Revenue,1, Revenue) over(order by [Year])), 'P2') as YOY_Growth
+from CTE
+
+
