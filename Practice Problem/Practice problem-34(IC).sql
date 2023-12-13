03cd96d527f7f1485009937b47bb51deead8350d
karan narula <karannarula370@gmail.com>
2023-12-13 19:56:39 +0530

    Uploaded data

---
+++ Practice Problem/Practice problem-34(IC).sql
@@ -0,0 +1,34 @@
+
+
+-- Add the new column to the table
+ALTER TABLE [dbo].[K1 & K3 (Excel)]
+ADD CFC_Pallet INT
+
+ALTER TABLE [dbo].[K1 & K3 (Excel)]
+ADD Pallet float;
+
+
+
+-- 1 .Update the values in the new column based on the join
+UPDATE [dbo].[K1 & K3 (Excel)] 
+SET CFC_Pallet = I.[CFC/Pallet]
+FROM [dbo].[K1 & K3 (Excel)] K
+LEFT OUTER JOIN [dbo].[Item Master] I ON K.[Brand Code] = I.Item;
+
+--2 Pallet
+update [dbo].[K1 & K3 (Excel)] 
+set Pallet = [Do Qty(Total CFC)] / CFC_Pallet
+from [dbo].[K1 & K3 (Excel)] 
+
+
+select * from [dbo].[K1 & K3 (Excel)]
+
+--- for view we can do this way
+select K.*, I.[CFC/Pallet] as [CFC/Pallet]
+from [dbo].[K1 & K3 (Excel)] K
+left outer join [dbo].[Item Master] I
+on K.[Brand Code] = I.Item
+
+select * from [dbo].[Item Master]
+
+
