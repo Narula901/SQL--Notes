eef635c9081c5520393004e13240f9cd202f6533
karan narula <karannarula370@gmail.com>
2023-12-26 15:24:19 +0530

    Uploaded data

--- Optimization of SQL Queries.sql
+++ Optimization of SQL Queries.sql
@@ -12,9 +12,18 @@ SalesOrderID,
 OrderDate
 )
 
-select OrderId, OrderDate 
+select OrderId, OrderDate     
 from [Sales].[Order]
 where year(OrderDate) = 2011
+
+/*
+select OrderId, OrderDate 
+from [Sales].[Order]
+where OrderDate >'2010-12-31' 
+and OrderDate < '2012-01-01'
+group by OrderId, OrderDate 
+having count(*)=1
+*/
 
 select * from #Sales2012
 
@@ -38,6 +47,21 @@ ProductCategory varchar(64)
 
 insert into #ProductsSold2011
 (
- 
+ SalesOrderID,
+ Orderdate,
+ LineTotal,
+ ProductID
 )
 
+select 
+A.SalesOrderID,
+A.OrderDate,
+B.LineTotal,
+B.ProductID
+
+from #Sales2012 A
+join [Sales].[OrderItem] B
+on A.SalesOrderID = B.OrderID
+
+
+select * from #ProductsSold2011
