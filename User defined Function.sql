27271c7a8eb581af0fd3032f82625eb04f499b7c
karan narula <karannarula370@gmail.com>
2024-02-01 15:53:51 +0530

    Uploaded data

--- User defined Function.sql
+++ User defined Function.sql
@@ -44,6 +44,101 @@ from [Sales].[Order] S
 where Year(S.OrderDate) = 2011
 
 
+
+/*
+Problem - Create a user-defined function that returns the percent that one number is of another.
+
+For example, if the first argument is 8 and the second argument is 10, the function should return the string "80.00%".
+
+The function should solve the "integer division" problem by allowing you to divide an integer by another integer, and yet get an accurate decimal result.
+*/
+
+create function dbo.RatioInterger(@Numerator int, @Denominator int)
+returns varchar(8)
+as
+begin
+
+declare @Decimal Float = (@Numerator * 1.0)/@Denominator
+return format(@Decimal, 'P')
+
+end
+
+select dbo.RatioInteger(3,5)
+-----------------------------------------------------
+create function dbo.Ratio(@Num int, @Deno int)
+returns varchar(8)
+as 
+begin
+	declare @Decimal float = 0;
+	declare @result varchar(8);
+
+	if @Deno = 0
+	begin
+		set @result = 'N/A'
+	end 
+	else
+	begin 
+	     set @Decimal =  (@Num * 1.0)/@Deno
+		 set @result = format(@Decimal, 'P')
+	end
+
+	return @result
+end
+
+select dbo.Ratio(3,0)
+
+
+---DROP FUNCTION dbo.ConvertToDateOnly;
+
+--------------------------------------------------------------
+create function dbo.ConvertToDateOnly(@Date Date)
+returns date
+as 
+begin
+		return cast(@Date as date)
+end
+cast(CreatedDate as date)
+
+/*
+Problem - Store the maximum amount of vacation time for any individual employee in a variable.
+
+Then create a query that displays all rows and the following columns from the AdventureWorks2019.HumanResources.Employee table:
+
+BusinessEntityID
+
+JobTitle
+
+VacationHours
+
+Then add a derived field called "PercentOfMaxVacation", 
+which returns the percent an individual employees' vacation hours are of the maximum vacation hours for any employee.
+*/
+
+Use AdventureWorksDW2019;
+
+declare @MaxVactionHours int = (select max(VacationHours) from [dbo].[DimEmployee])
+
+select EmployeeKey,VacationHours,
+PercentOfMaxVacation = dbo.Ratio(VacationHours, @MaxVactionHours)
+from  [dbo].[DimEmployee]
+
+-----------------------
+use [Jackson Dataset]
+go
+
+create or alter function dbo.ConvertToDateOnly(@Date Date)
+returns date
+as 
+begin
+		return cast(@Date as date)
+end
+
+go
+select *, GRNDateOnly = dbo.ConvertToDateOnly(GRNRegisteredDate) 
+from [dbo].[GRN Report]
+
+select * from [dbo].[GRN Report]
+
 /*
 Indexing: Ensure that your [dbo].[Calendar] table is properly indexed, 
 especially on the DateValue, WeekendFlag, and HolidayFlag columns.
