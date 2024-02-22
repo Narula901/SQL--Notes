e6868224fd259fe3ed9ef383f9d51c8a65d6d7e7
Narula901 <81371310+Narula901@users.noreply.github.com>
2024-02-22 15:08:09 +0530

    Update Practice Problem-36 (NorthWind DataBase).sql

--- Practice Problem/Practice Problem-36 (NorthWind DataBase).sql
+++ Practice Problem/Practice Problem-36 (NorthWind DataBase).sql
@@ -10,3 +10,16 @@ group by C.CategoryName
 select ProductID, ProductName,UnitsInStock, ReorderLevel
 from [dbo].[Products]
 where UnitsInStock < ReorderLevel
+
+select ProductID, ProductName,UnitsInStock, 
+UnitsOnOrder, ReorderLevel, Discontinued
+from [dbo].[Products]
+where (UnitsInStock + UnitsOnOrder)  <= ReorderLevel
+and 
+Discontinued = 0
+
+
+select CustomerID,  CompanyName, Region
+from [dbo].[Customers]
+group by CustomerID,  CompanyName, Region
+order by Region desc
