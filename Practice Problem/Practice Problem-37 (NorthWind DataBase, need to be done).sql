ed099335bc2abc0ecc604a5ba6f63250c3dd0ae9
Narula901 <81371310+Narula901@users.noreply.github.com>
2024-02-26 16:01:52 +0530

    Create Practice Problem-37 (NorthWind DataBase, need to be done).sql

---
+++ Practice Problem/Practice Problem-37 (NorthWind DataBase, need to be done).sql
@@ -0,0 +1,14 @@
+/*
+Problem - 29 We're doing inventory, and need to show information like the below, 
+for all orders. Sort by OrderID and Product ID.
+*/
+
+
+select * from [dbo].[Orders]
+select * from [dbo].[Order Details]
+select * from [dbo].[Products]
+select * from [dbo].[Employees]
+
+
+
+
